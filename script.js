aa5755682cd246c0b3f06a58c99c5307ea8e5435
Indu Prabha <apindhuece12@gmail.com>
2024-04-10 17:35:42 -0400

    done with q3

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ const n4 = 5;
  const Divby5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0
  console.log(Divby5); // true
 
- // Check if the first number is larger than the last.
+ // 2.Check if the first number is larger than the last.
  if (n1 > n4) {
  console.log("n1 is larger"); // true
  }
+
+ // 3.Subtract the first number from the second number.
+  console.log(n2 - n1 ); // -5
+//Multiply the result by the third number.
+  console.log(-5 * 10); //-50
+//Find the remainder of dividing the result by the fourth number.
+console.log(-50 % 5); // 0
