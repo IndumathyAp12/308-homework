8e43853b99987331dacbd3f0de6ea03421ceefa1
Indu Prabha <apindhuece12@gmail.com>
2024-04-10 01:04:31 -0400

    done with q2

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ const n4 = 5;
  const Divby5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0
  console.log(Divby5); // true
 
- // 2.
+ // Check if the first number is larger than the last.
+ if (n1 > n4) {
+ console.log("n1 is larger"); // true
+ }
