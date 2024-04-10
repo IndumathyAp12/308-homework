94a5662a305a0819c9fb331277f637baa1dab5a4
Indu Prabha <apindhuece12@gmail.com>
2024-04-10 00:43:36 -0400

    done with q1

---
+++ script.js
@@ -0,0 +1,10 @@
+// The initial numbers that must be verified.
+const n1 = 20;
+const n2 = 15;
+const n3 = 10;
+const n4 = 5;
+ // 1.Check if all numbers are divisible by 5.
+ const Divby5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0
+ console.log(Divby5); // true
+
+ // 2.
